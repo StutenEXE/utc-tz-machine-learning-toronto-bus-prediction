20e97c10e04ec1b7c8df9e1fa42c3a0bc0f888a8
StutenEXE <alexandre.bidaux78@gmail.com>
2026-01-03 10:41:25 +0100

    [prog] tested basic models on different scenarios

--- scripts/train_models_summer.py
+++ scripts/train_models_summer.py
@@ -3,12 +3,12 @@ from sklearn.model_selection import train_test_split
 from train_utils import *
 
 
-id=2 # id used to describe the model training scenario we are executing (see ./reports/training_results.md)
+id=3 # id used to describe the model training scenario we are executing (see ./reports/training_results.md)
 
-df = pd.read_csv('./data/4_preprocessed_dataset_winter.csv')
+df = pd.read_csv('./data/4_preprocessed_dataset_summer.csv')
 
 # Split between train set, and the combined test and val sets
-X_train, X_test_val, y_train, y_test_val = train_test_split(df.drop(columns=["DELAY_LOG1P"]), df["DELAY_LOG1P"], test_size=0.6, random_state=99)
+X_train, X_test_val, y_train, y_test_val = train_test_split(df.drop(columns=["DELAY_LOG1P"]), df["DELAY_LOG1P"], test_size=0.4, random_state=99)
 # Split the test and val sets in their respective sets
 X_val, X_test, y_val, y_test = train_test_split(X_test_val, y_test_val, test_size=0.5, random_state=66)
 
@@ -25,7 +25,7 @@ setValues(X_train, y_train, X_val, y_val, X_test, y_test)
 # Linear Regression Elasticnet testing
 # test_model(id, "Linear Regression (Elasticnet)", tune_params_linear_regression_elasticnet, create_linear_regression_elasticnet)
 # Random Forest testing (very slow)
-# test_model(id, "Random Forest", tune_params_random_forest, create_random_forest, n_trials=1)
+# test_model(id, "Random Forest", tune_params_random_forest, create_random_forest, n_trials=5)
 # Gradient Boosting testing
 # test_model(id, "Gradient Boosting", tune_params_gradient_boosting, create_gradient_boosting)
 # XGBoost testing
